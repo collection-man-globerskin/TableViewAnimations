b883f5968f4e82e5d49dae87e161ebe5cbcd3b65
collection-man-globerskin <julian.llorensi@globant.com>
2020-07-23 15:30:21 -0300

    Initial Commit

---
+++ TableViewAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewAnimations
+//
+//  Created by Julian Llorensi on 23/07/2020.
+//  Copyright © 2020 Julian Llorensi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
